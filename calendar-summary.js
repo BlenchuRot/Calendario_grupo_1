eeb3ae4fa6c2f3bef59b962faaa6a1059121a615
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-04-21 14:00:28 +0200

    calendar-summary

--- calendar-summary.js
+++ calendar-summary.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'https://cdn.pika.dev/lit-element';
+import { dateService } from './date-service.js';
 import { DateFormatter } from './date-formatter.js';
 
 class XCalendarSummary extends LitElement {
@@ -14,13 +15,23 @@ class XCalendarSummary extends LitElement {
         };
     }
     get monthString() {
-        return DateFormatter.monthString(dateService.date);
+        return DateFormatter.monthString(this.date);
     }
     constructor() {
         super();
         this.date = dateService.date;
     }
-    
+    connectedCallback() {
+        super.connectedCallback();
+        dateService.on(dateService.MONTH_CHANGED, this._onMonthChanged);
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        dateService.off(dateService.MONTH_CHANGED, this._onMonthChanged);
+    }
+    _onMonthChanged = (date) => {
+        this.date = date;
+    }
     render() {
         return html`
             <span class="x-calendar-summary">${this.monthString}</span>
