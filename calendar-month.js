b40c43efe692bd2aca376e4bd1a836482eff6de4
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-04-22 12:46:50 +0200

    navigation

---
+++ calendar-month.js
@@ -0,0 +1,114 @@
+import { LitElement, html, css } from 'https://cdn.pika.dev/lit-element';
+import { MonthHelper } from './month-helper.js';
+import { config } from './config.js';
+import { dateService } from './date-service.js'
+import { WEEKDAY_LETTERS } from './date-constants.js';
+
+import './calendar-day.js';
+
+const DAYS_PER_WEEK = 7;
+
+class XCalendarMonth extends LitElement {
+    static get styles() {
+        return css`
+        :host {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: repeat(7, 1fr);
+    gap: var(--x-margin-small);
+    justify-items: stretch;
+    font-size: var(--x-font-tiny);
+}
+
+x-calendar-day {
+    box-sizing: border-box;
+    cursor: pointer;
+    
+}
+
+x-calendar-day[data-outside]{
+    color: var(--x-color-primary--light);
+}
+
+x-calendar-day[data-today]{
+    background-color: var(--x-color-secondary);
+}
+
+x-calendar-day[data-selected]{
+    border: 1px solid var(--x-color-secondary);
+}
+
+.x-month__item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+        `;
+    }
+    get date() {
+        return new Date(this.dataset.date);
+    }
+    get days() {
+        return MonthHelper.getDays(this.date, config.startDay, DAYS_PER_WEEK * config.monthRows);
+    }
+   
+   
+    static get properties() {
+        return {
+            date: { type: string}
+        };
+    }
+    static get properties() {
+        return {
+            date: { type: string }
+        };
+    }
+    _onClick = (ev) => {
+        const newSelectedDay = this._findCalendarDay(ev.path);
+        if (!newSelectedDay) {
+            return;
+        }
+        this._selectedDay && this._selectedDay.removeAttribute('data-selected');
+        this._selectedDay = newSelectedDay;
+        this._selectedDay.setAttribute('data-selected', '');
+    }
+    _findCalendarDay(path) {
+        return path.find((el) => el.localName === 'x-calendar-day');
+    }
+    _getWeekDays() {
+        const days = [];
+        for (let delante = config.startDay; delante < WEEKDAY_LETTERS.length; delante++) {
+            days.push(WEEKDAY_LETTERS[delante]);
+        }
+        for (let detras = 0; detras < config.startDay; detras++) {
+            days.push(WEEKDAY_LETTERS[detras])
+        }
+        return days;
+    }
+    _renderDay(day) {
+        const attrs = [];
+        if (dateService.isToday(day)) {
+            attrs.push('data-today');
+        }
+        if (day.getMonth() !== this.date.getMonth()) {
+            attrs.push('data-outside');
+        }
+        return `<x-calendar-day @click={this._onClick} class="x-month__item" ${attrs} data-date="${day.toDateString()}"></x-calendar-day>`
+    }
+    _renderDays() {
+        return this.days.map((day) => this._renderDay(day))
+    }
+    _renderWeekdays() {
+        return this._getWeekDays().map((wd) => `<div @click={this._onClick} class="x-month__item">${wd}</div>`);
+    }
+    render() {
+        return html `
+           
+           ${this._renderWeekdays()}
+            ${this._renderDays()}
+        `;
+
+    }
+}
+
+customElements.define('x-calendar-month', XCalendarMonth);
--- calendar-navigation.js
+++ calendar-navigation.js
@@ -28,11 +28,11 @@ class XCalendarNavigation extends LitElement {
     render() {
         return html` 
       
-            <svg @click= "${this._onNextClick}"  class="x-navigation__icon" id="previous" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+            <svg @click= ${this._onNextClick} class="x-navigation__icon" id="previous" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
                <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/>
                 
            </svg>
-            <svg @click="${this._onPreviousClick}" class="x-navigation__icon" id="next" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+            <svg @click=${this._onPreviousClick} class="x-navigation__icon" id="next" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
                 <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
                  
             </svg>
