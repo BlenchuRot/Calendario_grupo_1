5f883b6392b7564b66a09da1b04288c561667372
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-04-23 13:58:19 +0200

    calendar

--- calendar.js
+++ calendar.js
@@ -1,1 +1,44 @@
 import { LitElement, html, css } from 'https://cdn.pika.dev/lit-element';
+import { dateService } from './date-service.js';
+import './calendar-header.js';
+import './calendar-body.js';
+
+
+class XCalendar extends LitElement{
+    static get styles(){
+        return css`
+        :host {
+    display: block;
+    background-color: var(--x-color-primary);
+    color: var(--x-color-text-primary);
+    width: 60%;
+    height: fit-content;
+    border: 1px solid var(--x-color-primary--light);
+}
+
+.x-calendar__header {
+    border-bottom: 1px solid var(--x-color-primary--light);
+}
+.x-calendar__body {
+    display: block;
+    padding: var(--x-padding-small);
+}`;
+    }
+    connectedCallback() {
+        super.connectedCallback();
+        dateService.start();
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+        dateService.stop();
+    }
+
+    render(){
+        return html`
+            <x-calendar-header class="x-calendar__header"></x-calendar-header>
+            <x-calendar-body class="x-calendar__body"></x-calendar-body>
+        `; 
+        
+    }
+}
+customElements.define('x-calendar', XCalendar);
